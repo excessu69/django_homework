2f7a4ff63cd42dade134258ff4d261ed8fd43051
excessu <pasynkov_93@bk.ru>
2024-08-26 19:20:00 +0400

    Inition commit

---
+++ 1.1-first-project/first_project/app/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from datetime import datetime
+import os
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('current_time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    files = os.listdir('.')
+    files_list = '<br>'.join(files)
+    msg = f'Рабочая директория:<br>{files_list}'
+    return HttpResponse(msg)
